d26abdf701cdf21da0c7bf11db61447fa4f21568
lifan30 <lifan30@maoyan.com>
2019-10-24 20:16:29 +0800

    这是第一次提交

--- lifan.js
+++ lifan.js
@@ -1,12 +1,1 @@
-
-// var a = true; if (a) {
-// function foo() { console.log("a"); } }
-// else {
-// function foo() { console.log("b"); }
-// }
-
-// foo();
-wwww
-nnnn
-var a='a  b   c';
-console.log(a.split(/\s+/g)
+这是第一次提交
